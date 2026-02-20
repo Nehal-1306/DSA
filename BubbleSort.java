b506fb2f41e0061596b1fccd18f6242ed9933f08
Nehal-1306 <nehalv906@gmail.com>
2026-02-20 18:07:51 +0530

    updated bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.*;
+public class BubbleSort {
+    public static void bubbleSort(int [] arr, int n){
+        for (int i=n-1;i>=0;i--){
+            for (int j=0;j<=i-1;j++){
+                if (arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+
+                }
+            }
+        }
+
+    }
+    public static void main(String [] args){
+        Scanner s =new Scanner (System.in);
+        int n=s.nextInt();
+        int [] arr= new int [n];
+        for (int i=0;i<n;i++){
+            arr[i]= s.nextInt();
+        }
+        bubbleSort(arr,n);
+System.out.println(Arrays.toString(arr));
+        }
+}
